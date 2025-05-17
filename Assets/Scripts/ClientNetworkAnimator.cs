5b9a5d436693be72596bf889b9531659e3191d47
OllieGuy <ollieguy02@gmail.com>
2025-05-17 20:40:39 +0100

    Animator working over network

--- Assets/Scripts/ClientNetworkAnimator.cs
+++ Assets/Scripts/ClientNetworkAnimator.cs
@@ -1,8 +1,22 @@
+using Unity.Netcode;
 using Unity.Netcode.Components;
 using UnityEngine;
 
 public class ClientNetworkAnimator : NetworkAnimator
 {
+    public NetworkVariable<float> NetSpeed = new NetworkVariable<float>(
+        writePerm: NetworkVariableWritePermission.Owner);
+
+    void Update()
+    {
+        if (IsOwner)
+        {
+            NetSpeed.Value = Animator.GetFloat("PlayerSpeed");
+        }
+
+        Animator.SetFloat("PlayerSpeed", NetSpeed.Value);
+    }
+
     protected override bool OnIsServerAuthoritative()
     {
         return false;
