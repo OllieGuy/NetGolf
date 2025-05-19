341a335251440173bd1a6e2b3510e0da22c0b377
OllieGuy <ollieguy02@gmail.com>
2025-05-19 17:51:10 +0100

    Working cosmetics

--- Assets/Scripts/PlayerCosmetics.cs
+++ Assets/Scripts/PlayerCosmetics.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class PlayerCosmetics : MonoBehaviour
 {
+    [SerializeField] SkinnedMeshRenderer playerRenderer;
+
     [SerializeField] Material[] skins;
     [SerializeField] Material[] accessoryColors;
 
@@ -15,36 +17,46 @@ public class PlayerCosmetics : MonoBehaviour
 
     void Start()
     {
+        SetTexture();
         AddAccessories();
+    }
+
+    void SetTexture()
+    {
+        int skinIndex = UnityEngine.Random.Range(0, skins.Length - 1);
+        playerRenderer.material = skins[skinIndex];
     }
 
     void AddAccessories()
     {
         if (UnityEngine.Random.Range(0, 100) < 10) // 10% face accessory chance
         {
-            int accessoryInList = UnityEngine.Random.Range(0, faceAccessories.Length);
-            GameObject accessory = Instantiate(faceAccessories[accessoryInList].gameObj, faceAccessoryTransform);
-            accessory.GetComponent<MeshRenderer>().material = faceAccessories[accessoryInList].texture == null ? AccessoryColour() : faceAccessories[0].texture;
+            int accessoryIndex = UnityEngine.Random.Range(0, faceAccessories.Length - 1);
+            GameObject accessory = Instantiate(faceAccessories[accessoryIndex].gameObj, faceAccessoryTransform);
+            accessory.transform.localPosition = Vector3.zero + faceAccessories[accessoryIndex].offset;
+            accessory.GetComponent<MeshRenderer>().material = faceAccessories[accessoryIndex].texture == null ? AccessoryColour() : faceAccessories[accessoryIndex].texture;
         }
 
         int headSeed = UnityEngine.Random.Range(0, 100);
         if (headSeed < 75) // 75% chance for hair
         {
-            int hairInList = UnityEngine.Random.Range(0, hairstyles.Length);
-            GameObject accessory = Instantiate(hairstyles[hairInList].gameObj, headAccessoryTransform);
-            accessory.GetComponent<MeshRenderer>().material = hairstyles[hairInList].texture == null ? AccessoryColour() : hairstyles[hairInList].texture;
+            int hairIndex = UnityEngine.Random.Range(0, hairstyles.Length - 1);
+            GameObject accessory = Instantiate(hairstyles[hairIndex].gameObj, headAccessoryTransform);
+            accessory.transform.localPosition = Vector3.zero + hairstyles[hairIndex].offset;
+            accessory.GetComponent<MeshRenderer>().material = hairstyles[hairIndex].texture == null ? AccessoryColour() : hairstyles[hairIndex].texture;
         }
         else if (headSeed >= 95) // 5% chance for hat
         {
-            int headAccessoryInList = UnityEngine.Random.Range(0, headAccessories.Length);
-            GameObject accessory = Instantiate(headAccessories[headAccessoryInList].gameObj, headAccessoryTransform);
-            accessory.GetComponent<MeshRenderer>().material = headAccessories[headAccessoryInList].texture == null ? AccessoryColour() : headAccessories[headAccessoryInList].texture;
+            int headAccessoryIndex = UnityEngine.Random.Range(0, headAccessories.Length - 1);
+            GameObject accessory = Instantiate(headAccessories[headAccessoryIndex].gameObj, headAccessoryTransform);
+            accessory.transform.localPosition = Vector3.zero + headAccessories[headAccessoryIndex].offset;
+            accessory.GetComponent<MeshRenderer>().material = headAccessories[headAccessoryIndex].texture == null ? AccessoryColour() : headAccessories[headAccessoryIndex].texture;
 
         }
 
         Material AccessoryColour()
         {
-            int colorInList = UnityEngine.Random.Range(0, accessoryColors.Length);
+            int colorInList = UnityEngine.Random.Range(0, accessoryColors.Length - 1);
             return accessoryColors[colorInList];
         }
     }
@@ -55,4 +67,5 @@ public class Accessory
 {
     public GameObject gameObj;
     public Material texture;
+    public Vector3 offset;
 }
